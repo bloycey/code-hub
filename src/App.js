65d1e5e32d20eab5ac61d5e34a8ecda17e7ca559
bloycey <bloyce1990@gmail.com>
2017-10-29 20:25:26 +1000

    Setting up app wrapper

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      
+      <a className="navbar-brand" href="#">Dashboard</a>
+      <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+        <form className="form-inline search-wrapper mt-2 mt-md-0">
+          <input className="form-control mr-sm-2 search-bar" type="text" placeholder="Search" aria-label="Search"/>>
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+  <div className="container-fluid">
+    <div className="row">
+      <nav className="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+        <ul className="nav nav-pills flex-column">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">Overview <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Reports</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Analytics</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Export</a>
+          </li>
+        </ul>
+
+        <ul className="nav nav-pills flex-column">
+          <li className="nav-item">
+            <a className="nav-link" href="#">Nav item</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Nav item again</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">One more nav</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Another nav item</a>
+          </li>
+        </ul>
+
+        <ul className="nav nav-pills flex-column">
+          <li className="nav-item">
+            <a className="nav-link" href="#">Nav item again</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">One more nav</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Another nav item</a>
+          </li>
+        </ul>
+      </nav>
+
+      <main role="main" className="col-sm-9 ml-sm-auto col-md-10 pt-3">
+        <h1>Dashboard</h1>
+
+      </main>
+    </div>
+  </div>
+  </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.6.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDs0LiNMQrvKwBPwM4Idp-DY-FMILARKKQ",
+    authDomain: "code-hub-f9773.firebaseapp.com",
+    databaseURL: "https://code-hub-f9773.firebaseio.com",
+    projectId: "code-hub-f9773",
+    storageBucket: "code-hub-f9773.appspot.com",
+    messagingSenderId: "96079154535"
+  };
+  firebase.initializeApp(config);
+</script>
